c7f5ea6e1b9104e7f5dffc8fae941569f6da1694
y24514 <y24514@iwate-it.ac.jp>
2025-11-20 16:04:02 +0900

    db comit

--- sportdataapp/db/sportdata_db.sql
+++ sportdataapp/db/sportdata_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2025-11-13 07:58:44
+-- 生成日時: 2025-11-20 08:03:15
 -- サーバのバージョン： 10.4.32-MariaDB
 -- PHP のバージョン: 8.2.12
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `calendar_tbl`
+--
+
+CREATE TABLE `calendar_tbl` (
+  `id` int(100) NOT NULL,
+  `group_id` varchar(100) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `memo` varchar(100) NOT NULL,
+  `startdate` date NOT NULL,
+  `enddate` date NOT NULL,
+  `create_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `calendar_tbl`
+--
+
+INSERT INTO `calendar_tbl` (`id`, `group_id`, `user_id`, `title`, `memo`, `startdate`, `enddate`, `create_at`) VALUES
+(4, 'cis', 'y24514', 'テスト', 'てすと', '2025-11-02', '2025-11-03', '2025-11-20 14:02:06');
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `goal_tbl`
 --
 
@@ -40,7 +64,7 @@ CREATE TABLE `goal_tbl` (
 --
 
 INSERT INTO `goal_tbl` (`goal_id`, `group_id`, `user_id`, `goal`, `created_at`) VALUES
-(4, 'cis', 'y24514', 'ストローク数と呼吸数を減らす', '2025-10-30 13:46:50');
+(4, 'cis', 'y24514', 'ストローク数を減らす', '2025-11-20 09:46:14');
 
 -- --------------------------------------------------------
 
@@ -85,40 +109,34 @@ CREATE TABLE `pi_tbl` (
   `create_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- テーブルの構造 `schedule_tbl`
+-- テーブルのデータのダンプ `pi_tbl`
 --
 
-CREATE TABLE `schedule_tbl` (
-  `id` int(11) NOT NULL,
-  `group_id` varchar(50) DEFAULT NULL,
-  `user_id` varchar(50) DEFAULT NULL,
-  `schedule_date` date NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `memo` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- テーブルのデータのダンプ `schedule_tbl`
---
-
-INSERT INTO `schedule_tbl` (`id`, `group_id`, `user_id`, `schedule_date`, `title`, `memo`, `created_at`) VALUES
-(1, 'cis', 'y24514', '2026-01-02', '練習試合', NULL, '2025-11-06 04:53:16');
+INSERT INTO `pi_tbl` (`id`, `group_id`, `user_id`, `height`, `weight`, `injury`, `sleeptime`, `create_at`) VALUES
+(24, 'cis', 'y24514', 170.0, 59.6, '', '08:00:00', '2025-11-17 11:14:36'),
+(29, 'cis', 'y24514', 170.0, 59.8, '左腕骨折', '09:00:00', '2025-11-17 11:27:34'),
+(31, 'cis', 'y24514', 172.0, 64.6, '', '05:00:00', '2025-11-18 09:43:57');
 
 --
 -- ダンプしたテーブルのインデックス
 --
 
 --
+-- テーブルのインデックス `calendar_tbl`
+--
+ALTER TABLE `calendar_tbl`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_id` (`group_id`,`user_id`),
+  ADD KEY `calendar_user` (`user_id`);
+
+--
 -- テーブルのインデックス `goal_tbl`
 --
 ALTER TABLE `goal_tbl`
   ADD PRIMARY KEY (`goal_id`),
-  ADD KEY `goal` (`user_id`),
-  ADD KEY `group_id` (`group_id`);
+  ADD KEY `goal_group` (`group_id`) USING BTREE,
+  ADD KEY `goal_user` (`user_id`) USING BTREE;
 
 --
 -- テーブルのインデックス `login_tbl`
@@ -133,20 +151,18 @@ ALTER TABLE `login_tbl`
 --
 ALTER TABLE `pi_tbl`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `group_id` (`group_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- テーブルのインデックス `schedule_tbl`
---
-ALTER TABLE `schedule_tbl`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `schedule_group_id` (`group_id`),
-  ADD KEY `schedule_user_id` (`user_id`);
+  ADD KEY `pi_group` (`group_id`) USING BTREE,
+  ADD KEY `pi_user` (`user_id`) USING BTREE;
 
 --
 -- ダンプしたテーブルの AUTO_INCREMENT
 --
+
+--
+-- テーブルの AUTO_INCREMENT `calendar_tbl`
+--
+ALTER TABLE `calendar_tbl`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- テーブルの AUTO_INCREMENT `goal_tbl`
@@ -164,38 +180,32 @@ ALTER TABLE `login_tbl`
 -- テーブルの AUTO_INCREMENT `pi_tbl`
 --
 ALTER TABLE `pi_tbl`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
---
--- テーブルの AUTO_INCREMENT `schedule_tbl`
---
-ALTER TABLE `schedule_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- ダンプしたテーブルの制約
 --
 
 --
+-- テーブルの制約 `calendar_tbl`
+--
+ALTER TABLE `calendar_tbl`
+  ADD CONSTRAINT `calendar_group` FOREIGN KEY (`group_id`) REFERENCES `login_tbl` (`group_id`),
+  ADD CONSTRAINT `calendar_user` FOREIGN KEY (`user_id`) REFERENCES `login_tbl` (`user_id`);
+
+--
 -- テーブルの制約 `goal_tbl`
 --
 ALTER TABLE `goal_tbl`
-  ADD CONSTRAINT `goal` FOREIGN KEY (`user_id`) REFERENCES `login_tbl` (`user_id`),
-  ADD CONSTRAINT `goal_group` FOREIGN KEY (`group_id`) REFERENCES `login_tbl` (`group_id`);
+  ADD CONSTRAINT `goal_group` FOREIGN KEY (`group_id`) REFERENCES `login_tbl` (`group_id`),
+  ADD CONSTRAINT `goal_user` FOREIGN KEY (`user_id`) REFERENCES `login_tbl` (`user_id`);
 
 --
 -- テーブルの制約 `pi_tbl`
 --
 ALTER TABLE `pi_tbl`
-  ADD CONSTRAINT `group_id` FOREIGN KEY (`group_id`) REFERENCES `login_tbl` (`group_id`),
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `login_tbl` (`user_id`);
-
---
--- テーブルの制約 `schedule_tbl`
---
-ALTER TABLE `schedule_tbl`
-  ADD CONSTRAINT `schedule_group_id` FOREIGN KEY (`group_id`) REFERENCES `login_tbl` (`group_id`),
-  ADD CONSTRAINT `schedule_user_id` FOREIGN KEY (`user_id`) REFERENCES `login_tbl` (`user_id`);
+  ADD CONSTRAINT `pi_group` FOREIGN KEY (`group_id`) REFERENCES `login_tbl` (`group_id`),
+  ADD CONSTRAINT `pi_user` FOREIGN KEY (`user_id`) REFERENCES `login_tbl` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
